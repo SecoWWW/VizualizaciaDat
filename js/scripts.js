f643cebd8fdb16eb817dc36fc7b495b65f756849
SecoWWW <patrik.scensny@gmail.com>
2018-03-21 16:38:17 +0100

    testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function createVerticies() {
-    var theta = (1 + Math.sqrt(5)/2);
+    var theta = (1 + Math.sqrt(5))/2;
     var reverse = 1/theta;
     var vertices = [
         1,1,1,
@@ -26,16 +26,27 @@ function createVerticies() {
         -theta,reverse,0,
         -theta,-reverse,0
     ]
+    for(var i = 0; i < vertices.length; i+=3){
+        console.log(i/3 + " : " + vertices[i] + " " + vertices[i+1] + " " + vertices[i+2]);
+    }        
     return vertices;
 }
 
 function createIndicies() {
     var indices = [
-        16, 0, 12,
-        12, 2, 17,
-        17, 12,16,
-
+        // 16, 0, 12,
+        // 12, 2, 17,
+        // 12, 17, 16
+        // 17, 12, 16, - wrong direction
         
+
+        0, 16, 1,
+        // 0, 8, 9,
+        9, 0, 1,
+
+        // 0, 8, 4,
+        // 0, 12, 14,
+        // 0, 14, 4
     ];
     return indices;
 }
