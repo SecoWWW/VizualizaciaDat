ff87eb8b73acb055a74ddd194b3aff54cc82f3da
Patrik Sčensný <patrik.scensny@gmail.com>
2018-03-22 00:36:28 +0100

    Modified indicies

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,26 @@
 function createVerticies() {
-    var theta = (1 + Math.sqrt(5))/2;
-    var reverse = 1/theta;
+    var theta = (1 + Math.sqrt(5)) / 2;
+    var reverse = 1 / theta;
     var vertices = [
-        1,1,1,
-        1,1,-1,
-        1,-1,1,
-        1,-1,-1,
-        -1,1,1,
-        -1,1,-1,
-        -1,-1,1,
-        -1,-1,-1,        
+        1, 1, 1,
+        1, 1, -1,
+        1, -1, 1,
+        1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, 1, -1, -1, -1,
 
-        0,theta,reverse,
-        0,theta,-reverse,
-        0,-theta,reverse,
-        0,-theta,-reverse,
+        0, theta, reverse,
+        0, theta, -reverse,
+        0, -theta, reverse,
+        0, -theta, -reverse,
 
-        reverse,0,theta,
-        reverse,0,-theta,
-        -reverse,0,theta,
-        -reverse,0,-theta,
+        reverse, 0, theta,
+        reverse, 0, -theta, -reverse, 0, theta, -reverse, 0, -theta,
 
-        theta,reverse,0,
-        theta,-reverse,0,
-        -theta,reverse,0,
-        -theta,-reverse,0
+        theta, reverse, 0,
+        theta, -reverse, 0, -theta, reverse, 0, -theta, -reverse, 0
     ]
-    for(var i = 0; i < vertices.length; i+=3){
-        console.log(i/3 + " : " + vertices[i] + " " + vertices[i+1] + " " + vertices[i+2]);
-    }        
+    for (var i = 0; i < vertices.length; i += 3) {
+        console.log(i / 3 + " : " + vertices[i] + " " + vertices[i + 1] + " " + vertices[i + 2]);
+    }
     return vertices;
 }
 
@@ -38,15 +30,15 @@ function createIndicies() {
         12, 2, 17,
         12, 17, 16,
         // 17, 12, 16, - wrong direction
-        
+
 
         0, 16, 1,
-        0, 8, 9,
+        8, 0, 9,
         9, 0, 1,
 
         0, 8, 4,
-        0, 12, 14,
-        0, 14, 4
+        14, 12, 0,
+        0, 4, 14
     ];
     return indices;
 }
